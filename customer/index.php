70bb10d298bd4b1630ca412d08f48d987a89b8cf
mwxgh <mwx6996@gmail.com>
2021-02-23 16:51:51 +0700

    update index

--- customer/index.php
+++ customer/index.php
@@ -328,26 +328,6 @@
             </div>
         </div>
         <!-- Popular Items End -->
-        <!--? Video Area Start -->
-        <div class="video-area">
-            <div class="container-fluid">
-                <div class="row align-items-center">
-                    <div class="col-lg-12">
-                    <div class="video-wrap">
-                        <div class="play-btn "><a class="popup-video" href="https://www.youtube.com/watch?v=KMc6DyEJp04"><i class="fas fa-play"></i></a></div>
-                    </div>
-                    </div>
-                </div>
-                <!-- Arrow -->
-                <div class="thumb-content-box">
-                    <div class="thumb-content">
-                        <h3>Next Video</h3>
-                        <a href="#"> <i class="flaticon-arrow"></i></a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Video Area End -->
         <!--? Watch Choice  Start-->
         <div class="watch-area section-padding30">
             <div class="container">
