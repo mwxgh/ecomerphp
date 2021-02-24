6a72d6918ebec9270752986232a55d8492a0a400
mwxgh <mwx6996@gmail.com>
2021-02-24 11:39:26 +0700

    php include

--- admin/navbar.php
+++ admin/navbar.php
@@ -7,7 +7,7 @@
       </p>
     </a>
     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownProfile">
-      <a class="dropdown-item" href="user.php">Profile</a>
+      <a class="dropdown-item" href="../user.php">Profile</a>
       <div class="dropdown-divider"></div>
         <a class="dropdown-item" href="../logout.php">Log out</a>
     </div>
